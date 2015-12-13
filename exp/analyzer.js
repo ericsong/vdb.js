4dcee1fff62788589128a9aedbb146c7f69989a5
ericsong <regonics@gmail.com>
2015-12-12 22:28:48 -0500

    new DS analyzer init

---
+++ exp/analyzer.js
@@ -0,0 +1,26 @@
+var tree = {
+    data: 'b',
+    left: {
+        data: {str: 'lol'},
+        left: null,
+        right: null
+    },
+    right: {
+        data: 'c',
+        left: null,
+        right: null
+    }
+}
+
+/*
+ * Check that all properties in 'props' array are properties of 'node'. Remove 'props' that are not present in 'node' 
+ */
+function checkProperties(node, props) {
+}
+
+function analyzer(root) {
+    var commonProperties = Object.getOwnPropertyNames(root);
+
+
+
+}
